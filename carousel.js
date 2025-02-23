0d1898e7dd29b74e7146f5b5fdc7fb64f48f77ac
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-23 18:21:22 +0000

    Implement Image Carousel

---
+++ carousel.js
@@ -0,0 +1,59 @@
+class Carousel{
+  constructor(imageContainer, imageSources, previousControl, nextControl) {
+    this.imageContainer = imageContainer;
+    this.images = this.createImageElements(imageSources);
+    this.previousControl = previousControl;
+    this.nextControl = nextControl;
+    this.currentImageIndex = 0;
+
+    this.initializeCarouselView();
+  }
+
+  initializeCarouselView() {
+    this.imageContainer.appendChild(this.images[this.currentImageIndex]);
+    this.addEventListenersToControls();
+  }
+
+  addEventListenersToControls() {
+    this.addClickEventListener(this.previousControl, -1);
+    this.addClickEventListener(this.nextControl, +1);
+  }
+
+  addClickEventListener(control, increment){
+    control.addEventListener("click", () => {
+
+      this.updateImageIndex(control, increment);
+
+
+      this.displayCurrentImage();
+    });
+  }
+
+  updateImageIndex(control, increment) {
+    this.currentImageIndex += increment;
+
+    if(control === this.previousControl && this.currentImageIndex === -1) {
+      this.currentImageIndex = this.images.length - 1;
+    } else if (control === this.nextControl &&  this.currentImageIndex === this.images.length) {
+      this.currentImageIndex = 0;
+    }
+  }
+
+  displayCurrentImage() {
+    this.imageContainer.removeChild(this.imageContainer.firstElementChild);
+    this.imageContainer.appendChild(this.images[this.currentImageIndex]);
+  }
+
+  createImageElements(imageSources) {
+    const images = [];
+
+    imageSources.forEach((imageSource) => {
+      const image = document.createElement("img");
+      image.src = imageSource;
+      images.push(image);
+    });
+
+    return images;
+  }
+}
+export default Carousel;
