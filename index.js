d422d62d157ec79c6ef2ca8244a9da492ba4d0f9
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-23 18:21:22 +0000

    Implement Image Carousel

---
+++ index.js
@@ -0,0 +1,10 @@
+import Carousel from "./carousel.js";
+
+const imageContainer = document.querySelector(".js-image-container");
+
+const imageSources = ["./images/_A642133.jpg", "./images/_A642237.jpg", "./images/_A643298.jpg", "./images/_A643303.jpg", "./images/DSC07016.jpg"];
+
+const previousControl = document.querySelector(".js-previous");
+const nextControl = document.querySelector(".js-next");
+
+new Carousel(imageContainer, imageSources, previousControl, nextControl);
